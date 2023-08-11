f473f2c8c2f7112941feba04af44437e511ffa96
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-10 22:59:42 -0400

    Allow certain roles to api calls

--- rest-security-employee-example/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ rest-security-employee-example/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -33,5 +37,24 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN")
 
+        );
+
+        // Use http basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable cross site request forgery (CSRF)
+        // in general, not requires for stateless rest APIs that use POST< PUT, DELETE / PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
 }
